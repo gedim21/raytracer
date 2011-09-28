f6a375d6a22b42c579dcdfdffe5bd661be75f7cc
gedim21 <gedim@gmail.com>
2011-09-28 03:31:47 +0300

    removed setters for Vector3's state

--- src/main/java/com/gedim/raytracer/util/Vector3.java
+++ src/main/java/com/gedim/raytracer/util/Vector3.java
@@ -64,23 +64,11 @@ public class Vector3 {
 		return x;
 	}
 
-	public void setX(double x) {
-		this.x = x;
-	}
-
 	public double getY() {
 		return y;
 	}
 
-	public void setY(double y) {
-		this.y = y;
-	}
-
 	public double getZ() {
 		return z;
-	}
-
-	public void setZ(double z) {
-		this.z = z;
 	}
 }
